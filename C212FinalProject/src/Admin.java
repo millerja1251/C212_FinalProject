f50ced67162ea9ae761643ccf57132dd1ed5e642
Miller, Jackson <jm122@iu.edu>
2021-04-30 19:11:39 -0400

    Create Admin.java

---
+++ C212FinalProject/src/Admin.java
@@ -0,0 +1,32 @@
+
+/*Jackson Miller
+ * jm122
+ * Enoch Wang
+ * enocwang
+ * 4/30/2021
+ * FinalPoject
+ */
+
+public class Admin {
+
+	private final String username;
+	private final String password;
+	
+	
+	public Admin() {
+		this.username = "MovieAdmin";
+		this.password = "c212s2021";
+	}
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+	
+}
